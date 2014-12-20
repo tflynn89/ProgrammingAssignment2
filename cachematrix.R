1bc2e922e06baa500eaaa887e719110aedd39fbc
tflynn89 <tflynn89@gmail.com>
2014-12-20 16:41:50 -0600

    Did the entire assignment including commenting

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This is for the second HW assignment 
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function is used to initially create the cacheMatrix object
 
+
+makeCacheMatrix <- function(x = matrix()) {      # input will be made into a matrix
+  i <- NULL 
+  
+  #  i is the inverse and resets to NULL with the creation of each Cache Matrix
+  
+  get <- function() { x }   # this function returns the value of original matrix
+  
+  setinverse <- function(inverse)  { i <<- inverse }
+  # this is called by cachesolve()
+  #  access and it will store the value using superassignment
+  
+  getinverse <- function() { i } # this will return the cached value to cachesolve() on
+  
+  
+  set <- function(y) {    # takes a matrix
+    x <<- y         # saves the matrix 
+    m <<- NULL      # resets the inverse to NULL
+  }
+  
+  list(get = get,          #  This is accessed each time makeCacheMatrix() is called,       
+       setinverse = setinverse,  #   that is, each time we make a new object.  This is a list of 
+       getinverse = getinverse,  #   the internal functions ('methods') so a calling function
+       set=set)     #   knows how to access those methods.      
+  
 }
 
+## This function is used to get and/or set the inverse of our cache Matrix object
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {   # the input x is an object created by makeCacheMatrix
+  i <- x$getinverse()               # accesses the object 'x' and gets the value of the inverse
+  if(!is.null(i)) {              # checks if inverse has already been defined
+    
+    message("getting cached data")  # ... send this message to the console
+    return(i)                       # ... and return the mean ... "return" ends 
+    
+  }
+  data <- i$get()        # we reach this code only if x$getinverse() returned NULL
+  i <- solve(data, ...)   # if i was NULL then we have to calculate the inverse
+  i$setinverse(i)           # store the calculated inverse value in x 
+  i               # return the mean to the code that called this function
 }
+
